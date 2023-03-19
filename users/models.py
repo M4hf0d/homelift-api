bee32fd7d7d06f60f7e69233bff9a95bbd5b082f
Sayah Mahfoud Abdel Ali <73748467+M4hf0d@users.noreply.github.com>
2023-03-19 16:43:31 +0100

    username exception fixed ✅✨✨✨✨✨🌙✨🌙

--- users/models.py
+++ users/models.py
@@ -1,5 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
+
+class CostumerManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        # create and save a new user with given email and password
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, username=None, **extra_fields):
+        # create and save a new superuser with given email and password
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('username', username)
+        return self.create_user(email, password, **extra_fields)
+
+
+
+# AUTH_PROVIDERS = {'facebook': 'facebook', 'google': 'google',
+#                   'twitter': 'twitter', 'email': 'email'}
+
 
 class Costumer(AbstractUser):
     username = models.CharField(blank=True, null=True, max_length=150 )
@@ -10,6 +35,12 @@ class Costumer(AbstractUser):
     created = models.DateField(auto_now_add = True) #Create 
     updated  = models.DateField(auto_now = True) #Update
     # profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    # auth_provider = models.CharField(
+    #     max_length=255, blank=False,
+    #     null=False, default=AUTH_PROVIDERS.get('email'))
+
+
+    objects = CostumerManager()
 
     USERNAME_FIELD = 'email'   # Auth with EmailField
     REQUIRED_FIELDS=['phone_number']
