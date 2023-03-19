5fc6d628204d56a977baf8d87b7dd6b1f1363ac3
Sayah Mahfoud Abdel Ali <73748467+M4hf0d@users.noreply.github.com>
2023-03-19 17:02:30 +0100

    CORS 🖐

--- homelift/settings.py
+++ homelift/settings.py
@@ -61,8 +61,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000"
 ]
 
-CORS_ALLOWED_ORIGINS_REGEXES = [
-    "http://localhost:3001",
+CORS_ORIGIN_WHITELIST = [
+     "http://localhost:3001",
     "http://localhost:3000",
     "http://127.0.0.1:3000"
 ]
