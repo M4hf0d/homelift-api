cf491cdb086f80533dba072c5f0b828cba400a2d
Abdeldjebbar10 <a.abid@esi-sba.dz>
2023-04-03 20:29:47 +0100

    up2

--- homelift/settings.py
+++ homelift/settings.py
@@ -176,12 +176,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend',
-    ]
-}
+g
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
