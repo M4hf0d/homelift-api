11acdb514b39ba02e5a5db72b35ae9113e8985b6
Chaima Slimane <c.slimane@esi-sba.dz>
2023-05-08 16:45:31 +0200

    modified

--- orders/serializers.py
+++ orders/serializers.py
@@ -5,36 +5,32 @@ from products.models import Product
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ['id','Cart_id','Product_id','Quantity','Created_at']   
-
-
-
-class AddToCartSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Item
-        fields=['Quantity']
-
+        fields = ['id','Cart_id','Product_id','Quantity','Created_at']  
+         
     def save(self,Cart_id,Product_id,Quantity):
-
+        
         item_queryset = Item.objects.filter(Product_id=Product_id,Cart_id=Cart_id)
 
         if item_queryset.exists():
             raise serializers.ValidationError("already Added , just modify the quantity or cancel")
-
+        
         if int(Quantity) > Product.objects.get(pk=Product_id).quantity :
             raise serializers.ValidationError("Unavailable quantity")
 
         item = Item(Cart_id=Cart.objects.get(pk=Cart_id),Quantity=Quantity,Product_id=Product.objects.get(pk=Product_id))
         item.save()
 
-        return item
-    
+        return item    
+
+
+
 class CartSerializer(serializers.ModelSerializer):
     Items = ItemSerializer(many=True,)
     class Meta:
         model = Cart
         fields = "__all__"
 
+ 
 # class AddToCartSerializer(serializers.ModelSerializer):
 #     class Meta:
 #         model=Item
@@ -53,4 +49,5 @@ class CartSerializer(serializers.ModelSerializer):
 #         item = Item(Cart_id=Cart.objects.get(pk=Cart_id),Quantity=Quantity,Product_id=Product.objects.get(pk=Product_id))
 #         item.save()
 
-#         return item    
+#         return item
+    
